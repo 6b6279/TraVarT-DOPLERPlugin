c3f4d185fe1a9dc0cbda0305f8a2f8214ea12078
Kaan B. Y. <109346960+6b6279@users.noreply.github.com>
2025-07-07 10:37:13 +0200

    Refactoring, embed benchmarking code

---
+++ src/main/java/edu/kit/travart/dopler/plugin/DoplerPlugin.java
@@ -0,0 +1,10 @@
+package edu.kit.travart.dopler.plugin;
+
+import org.pf4j.Plugin;
+import org.pf4j.PluginWrapper;
+
+public class DoplerPlugin extends Plugin {
+	public DoplerPlugin(final PluginWrapper wrapper) {
+		super(wrapper);
+	}
+}
---
+++ src/main/java/edu/kit/travart/dopler/transformation/DoplerBenchmarkingTransformer.java
@@ -0,0 +1,43 @@
+package edu.kit.travart.dopler.transformation;
+
+import at.jku.cps.travart.core.common.IStatistics;
+import at.jku.cps.travart.core.exception.NotSupportedVariabilityTypeException;
+import de.vill.model.FeatureModel;
+import edu.kit.dopler.model.Dopler;
+import edu.kit.travart.dopler.transformation.decision.to.feature.DmToFmTransformer;
+import edu.kit.travart.dopler.transformation.feature.to.decision.FmToDmTransformer;
+import at.jku.cps.travart.core.transformation.*;
+
+public class DoplerBenchmarkingTransformer extends AbstractBenchmarkingTransformer<Dopler> {
+
+	private final DmToFmTransformer dmToFmTransformer;
+    private final FmToDmTransformer fmToDmTransformer;
+
+    /**
+     * Constructor of {@link Transformer}.
+     *
+     * @param dmToFmTransformer {@link DmToFmTransformer}
+     * @param fmToDmTransformer {@link FmToDmTransformer}
+     */
+    public DoplerBenchmarkingTransformer(DmToFmTransformer dmToFmTransformer, FmToDmTransformer fmToDmTransformer) {
+        this.dmToFmTransformer = dmToFmTransformer;
+        this.fmToDmTransformer = fmToDmTransformer;
+    }
+
+	@Override
+	public Dopler transformInner(FeatureModel model, String modelName, STRATEGY strategy)
+			throws NotSupportedVariabilityTypeException {
+		return fmToDmTransformer.transform(model, strategy);
+	}
+	
+	@Override
+	public FeatureModel transformInner(Dopler model, String modelName, STRATEGY strategy)
+			throws NotSupportedVariabilityTypeException {
+		return dmToFmTransformer.transform(model, strategy);
+	}
+	
+	@Override
+	public IStatistics<Dopler> getTargetStatistics() {
+		throw new UnsupportedOperationException("Not implemented");
+	}
+}
